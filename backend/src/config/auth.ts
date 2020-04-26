124b1c9caf0312a6a2e32d7dee412908e2eba7aa
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-04-26 20:54:27 -0300

    feat: add routes authenticate

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a2294e8e673b9b79e8b9e4fe91153e75',
+    expiresIn: '1d',
+  },
+};
--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -32,9 +33,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, 'a2294e8e673b9b79e8b9e4fe91153e75', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
